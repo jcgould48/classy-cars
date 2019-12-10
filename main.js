37b91f99fef844dab4c602c8449282e9c7dbc4a7
Jesse Gould <jesse.gould@codeimmersives.com>
2019-12-10 15:17:58 -0500

    adds some solutions

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
 class Vehicle {
+constructor (name, wheelCount, maxSpeed){
+this.name = name;
+this.wheelCount = 4;
+this.maxSpeed = maxSpeed;
+this.speed = 0 ;
+}
+accelerate(){
+  if ((this.maxSpeed - this.speed.value)< (this.maxSpeed/3)){
+    this.speed = this.maxSpeed;
+  }
+  else {
+    this.speed = this.speed.value + maxSpeed/3
+  }
+}
 }
 
 class Car {
